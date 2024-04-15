b63705d6739bd0a1cabfa96f5900e11e27ab65fc
eliasBossen <eliapa@kth.se>
2024-04-15 15:49:17 +0200

    Bugs

--- include/character.h
+++ include/character.h
@@ -3,7 +3,7 @@
 
 typedef struct character Character;
 
-Character *createCharacter(int x, int y, SDL_Renderer *pRenderer, int window_width, int window_height);
+Character *createCharacter(int x, int y, SDL_Renderer *pRenderer, int window_w, int window_h);
 void updateCharacter(Character *pCharacter);
 void drawCharacter(Character *pCharacter);
 void destroyCharacter(Character *pCharacter);
--- src/character.c
+++ src/character.c
@@ -11,12 +11,12 @@ struct character{
     SDL_Rect characterRect;
 };
 
-Character *createCharacter(int x, int y, SDL_Renderer *pRenderer, int window_width, int window_height){
+Character *createCharacter(int x, int y, SDL_Renderer *pRenderer, int window_w, int window_h){
     Character *pCharacter = malloc(sizeof(struct character));
     pCharacter->x_vel = 0;
     pCharacter->y_vel = 0;
-    pCharacter->window_width = window_width;
-    pCharacter->window_height = window_height;
+    pCharacter->window_width = window_w;
+    pCharacter->window_height = window_h;
     SDL_Surface *pSurface = IMG_Load("resources/Ship.png");
     if (!pSurface){
         printf("Error: %s\n", SDL_GetError());
