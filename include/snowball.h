4b049598ac265fede5863d1b2a8b0f3d906ad38b
eliasBossen <eliapa@kth.se>
2024-04-17 16:55:39 +0200

    funktioner

--- include/snowball.h
+++ include/snowball.h
@@ -1,2 +1,9 @@
 #ifndef snowball_h
 #define snowball_h
+
+typedef struct snowball Snowball;
+
+Snowball *createSnowbal(int x , int y, int x_vel , int y_vel , SDL_Renderer *pRenderer, int window_w, int window_h);
+void updateSnowball(Snowball *pSnowball);
+void drawSnowball(Snowball *pSnowball);
+void destroySnowball(Snowball *pSnowball);
--- src/snowball.c
+++ src/snowball.c
@@ -1,3 +1,13 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
-#include "snoball.h"
+#include "snowball.h"
+
+
+struct snowball{
+    float x_pos, y_pos, x_vel, y_vel;
+    int window_width, window_height;
+
+    SDL_Renderer *pRenderer;
+    SDL_Texture *pTexture;
+    SDL_Rect snowballRect;
+};
