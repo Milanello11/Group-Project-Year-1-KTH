e8122e2454bcdeb7a2b7b3ce645f9fefe77da71a
Max Masuch <maxmasuch@AirsomtllhorMax.lan>
2024-04-19 11:28:16 +0200

    Ändra till 50*50

--- include/map.h
+++ include/map.h
@@ -2,7 +2,7 @@
 #define map_h
 
 
-void renderBackground(SDL_Renderer *pGameRender, SDL_Texture *tile_texture, SDL_Rect tiles_type[], SDL_Rect tile_placement[32][32]);
+void renderBackground(SDL_Renderer *pGameRender, SDL_Texture *tile_texture, SDL_Rect tiles_type[], SDL_Rect tile_placement[50][50]);
 int getTileValue(int x, int y);
 
 #endif
